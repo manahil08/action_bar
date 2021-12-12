8e7fbfb60f03cc1c1918c8b220f42b03aee9defb
manahil <bitf18a507@pucit.edu.pk>
2021-12-12 22:13:44 +0500

    third

--- app/src/main/java/com/example/app_bar/MainActivity.java
+++ app/src/main/java/com/example/app_bar/MainActivity.java
@@ -31,15 +31,11 @@ private Toolbar toolbar;
 
        right.setOnClickListener(new View.OnClickListener() {
             @Override
-           
+           git remote add origin https://github.com/manahil08/action_bar.git
                 Toast.makeText(MainActivity.this,"you clicked share button",Toast.LENGTH_SHORT).show();
 
                 Intent sendd=new Intent();
-                sendd.setAction(Intent.ACTION_SEND);
-                sendd.putExtra(Intent.EXTRA_TEXT,"hi,i have selected share button from my AppBar");
-                sendd.setType("text/plain");
-                Intent sharee=Intent.createChooser(sendd,null);
-                startActivity(sharee);
+          
             }
         });
        txt.setText("My App bar");
