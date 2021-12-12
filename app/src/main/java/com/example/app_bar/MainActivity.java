00cb43695e220098d4f1e8a6e5bbd35ee9e67ee4
manahil <bitf18a507@pucit.edu.pk>
2021-12-12 22:16:00 +0500

    nith

--- app/src/main/java/com/example/app_bar/MainActivity.java
+++ app/src/main/java/com/example/app_bar/MainActivity.java
@@ -36,11 +36,13 @@ private Toolbar toolbar;
 
                 Intent sendd=new Intent();
 
-                
+
                 sendd.setAction(Intent.ACTION_SEND);
                 sendd.putExtra(Intent.EXTRA_TEXT,"hi,i have selected share button from my AppBar");
                 sendd.setType("text/plain");
                 Intent sharee=Intent.createChooser(sendd,null);
+
+                
                 startActivity(sharee);
             }
         });
